c8864eaaabcf22f879921b41bb4808d6a5e2628c
duyph15719 <duyncph15719@fpt.edu.vn>
2022-10-07 12:32:34 +0700

    Created: Dashboard (Frontend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Router } from 'express';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Layoutclient from './pages/Layout/Layoutclient';
+import { listCategory } from './api/Categories';
+import { listUser } from './api/User';
+import AdminLayout from './layouts/AdminLayout';
+import WebLayout from './layouts/webLayout';
+import { ICategori } from './models/Categories';
+import { IUsers } from './models/User';
+import ErrorsWeb from './pages/404/errorsWeb';
+import Categories from './pages/admin/Categories/Categories';
+import CategoriesAdd from './pages/admin/Categories/CategoriesAdd';
+import CategoriesEdit from './pages/admin/Categories/CategoriesEdit';
+import Dashbroad from './pages/admin/dashbroad';
+import Users from './pages/admin/User/User';
+import UserAdd from './pages/admin/User/UserAdd';
+import UserEdit from './pages/admin/User/UserEdit';
 
+import Cart from './pages/Website/Cart/Cart';
+import Pay from './pages/Website/Pay/Pay';
 
 function App() {
+  const [user, setUser] = useState<IUsers[]>([])
+  const [categories, setCategories] = useState<ICategori[]>([])
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await listUser();
+      setUser(data)
+    }
+    getUser();
+  }, []);
+  useEffect(() => {
+    const getCategories = async () => {
+      const { data } = await listCategory();
+      setCategories(data)
+    }
+    getCategories();
+  }, []);
   return (
-<Routes>
-  <Route path= {'/'} element ={< Layoutclient />}>
-    <Route index element = {< Homepage />} />
-  </Route>
-</Routes>
+    <div className="App">
+      <Routes>
+        <Route path="" element={<WebLayout />} >
+          <Route path='cart' element={<Cart />} />
+          <Route path='pay' element={<Pay />} />
+        </Route>
+        <Route path="admin" element={<AdminLayout />}>
+          <Route index element={<Dashbroad />} />
+          <Route path="categories">
+            <Route index element={<Categories categories={categories} />} />
+            <Route path="add" element={<CategoriesAdd />} />
+            <Route path=":id/edit" element={<CategoriesEdit />} />
+          </Route>
+          <Route path="user">
+            <Route index element={<Users user={user} />} />
+            <Route path="add" element={<UserAdd />} />
+            <Route path=":id/edit" element={<UserEdit />} />
+          </Route>
+          <Route path="*" element={<ErrorsWeb />} />
+        </Route>
+
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/layouts/webLayout.tsx
+++ frontend/src/layouts/webLayout.tsx
@@ -1,13 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import Header from '../pages/Website/header/header'
 
 type Props = {}
 
 const WebLayout = (props: Props) => {
   return (
-    <><header>
-      <Header />
-    </header>
+    <>
       <div>
         <main>
           <Outlet />
