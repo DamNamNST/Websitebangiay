339bb86c0804734109d8ef9662635e5650330eae
Viet Anh Ngo <ngovietanh503@gmail.com>
2022-10-21 17:46:34 +0700

    api:size

--- frontend/src/api/size.tsx
+++ frontend/src/api/size.tsx
@@ -1,1 +1,27 @@
 import instance from "./instance";
+
+
+export const listSize: any = () => {
+    const url = `/size`;
+    return instance.get(url);
+}
+
+export const getSizeId: any = (id: any) => {
+    const url = `/size/${id}`;
+    return instance.get(url);
+}
+
+export const add: any = (product: any) => {
+    const url = `/size`;
+    return instance.post(url, product);
+}
+
+export const editSize: any = (product: any) => {
+    const url = `/size/${product.id}`;
+    return instance.put(url, product);
+}
+
+export const removeSize: any = (id: any) => {
+    const url = `/size/${id}`;
+    return instance.delete(url);
+}   
