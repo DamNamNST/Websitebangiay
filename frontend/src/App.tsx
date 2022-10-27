dd98da5c63210c5b3c207ba3cfefa2c1956c4d66
tung3122002 <tung3122002@gmail.com>
2022-10-27 14:16:23 +0700

    up api Banner date 27/10

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Layoutclient from "./Layout/Layoutclient";
+
 import "antd/dist/antd.css";
 import About from "./pages/About/About";
 import Products from "./pages/ProductList/products";
 import Lienhe from "./pages/lienhe";
 import { ICategori } from "./models/Categories";
 import { IUsers } from "./models/User";
-import { listUser } from "./api/User";
-import { listCategory } from "./api/Categories";
+
+
 import Cart from "./pages/Website/Cart/Cart";
 import Pay from "./pages/Website/Pay/Pay";
 import ErrorsWeb from "./pages/404/errorsWeb";
 import NewsPage from "./pages/News/News";
 import NewsDetail from "./pages/News/Newsdetail";
 import Purchased from "./pages/Purchased";
+
+
+import { listCategory } from "./api/Categories";
+import Layoutclient from "./Layout/Layoutclient";
+import { listUser } from "./api/User";
 
 function App() {
   const [user, setUser] = useState<IUsers[]>([]);
