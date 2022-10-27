ea01bb1481e93e5cee623864c70f043c39727355
Duc Mai Thanh <ducmtph14090@fpt.edu.vn>
2022-10-27 16:25:48 +0700

    categorry

--- frontend/src/pages/Category/Category.tsx
+++ frontend/src/pages/Category/Category.tsx
@@ -1,31 +1,81 @@
 import React from 'react'
 import './Category.css'
+import { Space, Table, Tag } from 'antd';
+const { Column, ColumnGroup } = Table;
+
+interface DataType {
+  key: React.Key;
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
+
+const data: DataType[] = [
+  {
+    key: '1',
+    firstName: 'John',
+    lastName: 'Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    firstName: 'Jim',
+    lastName: 'Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    firstName: 'Joe',
+    lastName: 'Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
 type Props = {}
 
 const Category = (props: Props) => {
     return (
         
-        <div className="max-w-7xl mx-auto text-white grid md:grid-cols-3 grid-cols-1 gap-4 pt-10  ">
-            <div className="box  text-center">
-                <div className="flex justify-center"><img className="" src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/title_block_05.png" alt="" /></div>
-                <div className="flex justify-center"><img src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/product_block_03.jpg" alt="" /></div>
-                <button className='  xemthem'>Xem Sản Phẩm</button>
-
-                <div className="showbox slideright">box 1</div>
-            </div>
-            <div className="box  text-center">
-                <div className="flex justify-center"><img className="" src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/title_block_05.png" alt="" /></div>
-                <div className="flex justify-center"><img src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/product_block_03.jpg" alt="" /></div>
-                <button className='  xemthem'>Xem Sản Phẩm</button>
-                <div className="showbox slideright">box 1</div>
-            </div>
-            <div className="box  text-center">
-                <div className="flex justify-center"><img className="" src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/title_block_05.png" alt="" /></div>
-                <div className="flex justify-center"><img src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/product_block_03.jpg" alt="" /></div>
-                <button className='  xemthem'>Xem Sản Phẩm</button>
-                <div className="showbox slideright">box 1</div>
-            </div>
-        </div>
+        <Table dataSource={data}>
+        <ColumnGroup title="Name">
+          <Column title="First Name" dataIndex="firstName" key="firstName" />
+          <Column title="Last Name" dataIndex="lastName" key="lastName" />
+        </ColumnGroup>
+        <Column title="Age" dataIndex="age" key="age" />
+        <Column title="Address" dataIndex="address" key="address" />
+        <Column
+          title="Tags"
+          dataIndex="tags"
+          key="tags"
+          render={(tags: string[]) => (
+            <>
+              {tags.map(tag => (
+                <Tag color="blue" key={tag}>
+                  {tag}
+                </Tag>
+              ))}
+            </>
+          )}
+        />
+        <Column
+          title="Action"
+          key="action"
+          render={(_: any, record: DataType) => (
+            <Space size="middle">
+              <a>Invite {record.lastName}</a>
+              <a>Delete</a>
+            </Space>
+          )}
+        />
+      </Table>
     )
 }
 
