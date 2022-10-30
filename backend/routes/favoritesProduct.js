8f268c4f111cb994b5aa907db9e21e9f67498d23
Duc Mai Thanh <ducmtph14090@fpt.edu.vn>
2022-10-30 13:04:07 +0700

    feach:voucher

--- backend/routes/favoritesProduct.js
+++ backend/routes/favoritesProduct.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { create, list, read, remove, update } from "../controllers/favoritesProduct";
 import { userById } from "../controllers/user";
-import { isAuth, requireSignin } from "../middlewares/checkAuth";
+import { isAuth, requireSignin } from "../middlewares/checkUser";
 
 const router = Router();
 
